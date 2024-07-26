b9b61ad84878266c1c001ac0cd77baec212a09d6
InaamMirzaAati <inaammirza48@gmail.com>
2024-07-26 20:14:12 +0500

    Landing Page Improvements

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,4 +1,4 @@
-import { LandingNavbar } from "@/components/landing-navbar";
+
 import { LandingHero } from "@/components/landing-hero";
 import { LandingContent } from "@/components/landing-content";
 import Youtube from "../../components/landingpage/Youtube";
@@ -6,12 +6,12 @@ import CheckListing from "../../components/landingpage/CheckListing";
 import Faqs from "../../components/landingpage/Faqs";
 import Testimonials from "../../components/landingpage/Testimonials";
 import Methodlogy from "../../components/landingpage/Methodlogy";
-import Footer from "../../components/landingpage/Footer";
+
 
 const LandingPage = () => {
   return (
     <div className="h-full w-full bg-[#2A507E]">
-      <LandingNavbar />
+     
       <LandingHero />
       <Methodlogy/>
       <Testimonials/>
@@ -19,9 +19,9 @@ const LandingPage = () => {
       <CheckListing />
       <Faqs />
       {/* <LandingContent /> */}
-      <Footer/>
+      
     </div>
-  );
+  ); 
 };
 
 export default LandingPage;
--- components/landingpage/Testimonials.tsx
+++ components/landingpage/Testimonials.tsx
@@ -1,127 +1,30 @@
 "use client";
-import React, { useState } from "react";
-import { PieChart, Pie, Sector, ResponsiveContainer } from "recharts";
+import React from "react";
 import Image from "next/image";
 import ali from "../../public/ali.jpg";
 import arthur from "../../public/arthur.jpg";
 import kyle from "../../public/kyle.png";
 import yasmin from "../../public/jasmin.png";
-
-const data = [
-  { name: "Group A", value: 400, fill: "#92B8FF" },
-  { name: "Group B", value: 300, fill: "#001226" },
-  { name: "Group C", value: 300, fill: "#2D4778" },
-];
-interface CustomShapeProps {
-  midAngle: number;
-  cx: number;
-  cy: number;
-  innerRadius: number;
-  outerRadius: number;
-  startAngle: number;
-  endAngle: number;
-  fill: string;
-  payload: {
-    name: string;
-  };
-  percent: number;
-  value: number;
-}
-
-const renderActiveShape = (props: CustomShapeProps) => {
-  const RADIAN = Math.PI / 180;
-  const {
-    cx,
-    cy,
-    midAngle,
-    innerRadius,
-    outerRadius,
-    startAngle,
-    endAngle,
-    fill,
-    payload,
-    percent,
-    value,
-  } = props;
-  const sin = Math.sin(-RADIAN * midAngle);
-  const cos = Math.cos(-RADIAN * midAngle);
-  const sx = cx + (outerRadius + 10) * cos;
-  const sy = cy + (outerRadius + 10) * sin;
-  const mx = cx + (outerRadius + 30) * cos;
-  const my = cy + (outerRadius + 30) * sin;
-  const ex = mx + (cos >= 0 ? 1 : -1) * 22;
-  const ey = my;
-  const textAnchor = cos >= 0 ? "start" : "end";
-
-  return (
-    <g>
-      <text x={cx} y={cy} dy={8} textAnchor="middle" fill={fill}>
-        {payload.name}
-      </text>
-      <Sector
-        cx={cx}
-        cy={cy}
-        innerRadius={innerRadius}
-        outerRadius={outerRadius}
-        startAngle={startAngle}
-        endAngle={endAngle}
-        fill={fill}
-      />
-      <Sector
-        cx={cx}
-        cy={cy}
-        startAngle={startAngle}
-        endAngle={endAngle}
-        innerRadius={outerRadius + 6}
-        outerRadius={outerRadius + 10}
-        fill={fill}
-      />
-      <path
-        d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`}
-        stroke={fill}
-        fill="none"
-      />
-      <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
-      <text
-        x={ex + (cos >= 0 ? 1 : -1) * 12}
-        y={ey}
-        textAnchor={textAnchor}
-        fill="#333"
-      >{`PV ${value}`}</text>
-      <text
-        x={ex + (cos >= 0 ? 1 : -1) * 12}
-        y={ey}
-        dy={18}
-        textAnchor={textAnchor}
-        fill="#999"
-      >
-        {`(Rate ${(percent * 100).toFixed(2)}%)`}
-      </text>
-    </g>
-  );
-};
+import CountUp from "react-countup";
+import StarRatings from "react-star-ratings";
 
 const Testimonials = () => {
-  const [activeIndex, setActiveIndex] = useState(0);
-
-  const onPieEnter = (_: any, index: number) => {
-    setActiveIndex(index);
-  };
-
   const testimonials = [
     {
       text: `I'm gonna be real. I had no clue what I was doing. Karki’s methodology got me from my middling
                   diagnostic to a 516 on my actual test, all within three months.`,
       name: "Yasmin Maurice",
       description: "516, Tulane",
-      image:yasmin,
+      image: yasmin,
+      rating: 5,
     },
     {
       text: `My first test was SO stressful and chaotic. I did poorly. On my retake, I needed a plan.
                   Karki developed one that got me a score I would’ve never imagined possible.`,
       name: "Ali Nassari",
       description: "517, UT Austin",
-      image:ali,
+      image: ali,
+      rating: 5,
     },
     {
       text: `Karki’s software created a personalized strategy to help me succeed, planning my days for me. On my
@@ -129,7 +32,8 @@ const Testimonials = () => {
                 `,
       name: "Kyle Peeples",
       description: "518, Tulane",
-      image:kyle,
+      image: kyle,
+      rating: 5,
     },
     {
       text: `Brilliant, eager to help, hilarious, with an infectious energy that just inspired me to keep
@@ -137,69 +41,74 @@ const Testimonials = () => {
                   Couldn’t recommend Karki more.`,
       name: `Arthur Cowan`,
       description: "519, SDSU",
-      image:arthur,
+      image: arthur,
+      rating: 5,
     },
-    // Add more testimonials as needed
   ];
 
   return (
     <section className="bg-white py-16">
       <div className="container mx-auto px-6">
         <div className="text-center mb-10">
-          <h1
-            className="text-4xl md:text-5xl font-bold mb-4"
-            style={{ color: "#0E2247" }}
-          >
+          <h1 className="text-4xl md:text-5xl font-bold mb-4" style={{ color: "#0E2247" }}>
             Testimonials
           </h1>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 items-center">
-          <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-8">
+        <div className="w-full md:w-[60%] m-auto pb-10 text-center">
+          <p className="text-black text-xl mb-3">Meow there!</p>
+          <p className="text-black text-xl mb-3">
+            Kalypso is an MCAT tech company and social business built by premeds for premeds, focused on{" "}
+            <strong className="font-bold">innovation & accessibility.</strong>
+          </p>
+          <p className="text-black text-xl mb-3">
+            We use the latest technology and algorithms to deliver your potential.{" "}
+          </p>
+          <p className="text-black text-xl mb-3">
+            Because your patients deserve only the best.
+          </p>
+        </div>
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+          <div className="px-2 text-center col-span-1">
+            <div className="mt-20">
+              <div className="text-center text-5xl font-bold">
+                <CountUp start={0} end={14} duration={2.75} suffix=" +" />
+              </div>
+              <p className="text-black text-2xl mb-0">Average increase in beta</p>
+            </div>
+            <div className="mt-20">
+              <div className="text-center text-5xl font-bold">
+                <CountUp start={0} end={514} duration={2.75} suffix=" +" />
+              </div>
+              <p className="text-black text-2xl mb-0">Average score in beta</p>
+            </div>
+          </div>
+          <div className="col-span-1 lg:col-span-2 grid grid-cols-1 md:grid-cols-2 gap-8">
             {testimonials.map((testimonial, index) => (
-              <div
-                key={index}
-                className="w-full mx-auto bg-white border shadow-md rounded-lg overflow-hidden"
-              >
+              <div key={index} className="w-full mx-auto bg-white border shadow-md rounded-lg overflow-hidden">
                 <div className="px-6 py-3">
-                  <p className="mt-4  text-sm  text-black font-medium mb-4 min-h-[110px]">
-                    {testimonial.text}
-                  </p>
-                  <div className="flex items-center">
-                    <Image
-                      className="w-10 h-10 rounded-full mr-2"
-                      src={testimonial.image}
-                      alt="Avatar"
-                    />
-                    <div className="text-sm">
-                      <p className="text-black font-medium mb-0">
-                        {testimonial.name}
-                      </p>
-                      <p className="text-gray-400 text-[10px]">
-                        {testimonial.description}
-                      </p>
+                  <p className="mt-4 text-sm text-black font-medium mb-4 min-h-[80px]">{testimonial.text}</p>
+                  <div className="flex justify-between items-center">
+                    <div className="flex items-center">
+                      <Image className="w-10 h-10 rounded-full mr-2" src={testimonial.image} alt="Avatar" />
+                      <div className="text-sm">
+                        <p className="text-black font-medium mb-0">{testimonial.name}</p>
+                        <p className="text-gray-400 text-[10px]">{testimonial.description}</p>
+                      </div>
+                    </div>
+                    <div>
+                      <StarRatings
+                        rating={testimonial.rating}
+                        starRatedColor="gold"
+                        numberOfStars={5}
+                        name="rating"
+                        starDimension="20px"
+                        starSpacing="2px"
+                      />
                     </div>
                   </div>
                 </div>
               </div>
             ))}
-          </div>
-
-          <div className="flex justify-center mt-6 md:mt-0">
-            <ResponsiveContainer width={400} height={400}>
-              <PieChart>
-                <Pie
-                  activeIndex={activeIndex}
-                  activeShape={renderActiveShape as any}
-                  data={data}
-                  cx="50%"
-                  cy="50%"
-                  innerRadius={60}
-                  outerRadius={80}
-                  dataKey="value"
-                  onMouseEnter={onPieEnter}
-                />
-              </PieChart>
-            </ResponsiveContainer>
           </div>
         </div>
       </div>
