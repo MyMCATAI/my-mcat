1324808ee97d68310dd8c12586eb670152bf9fa9
Prynce Karki <prynce@mymcat.ai>
2024-07-27 00:25:54 -0500

    main
    	modified:   components/landing-hero.tsx
    	modified:   components/landing-navbar.tsx
    	modified:   components/landingpage/Methodlogy.tsx
    	modified:   components/pie-chart.tsx

--- components/pie-chart.tsx
+++ components/pie-chart.tsx
@@ -21,16 +21,12 @@ const SimplePieChart: React.FC<SimplePieChartProps> = ({ data }) => {
         const angle = (value / total) * 360;
         const largeArcFlag = angle > 180 ? 1 : 0;
         const endAngle = startAngle + angle;
-
         const x1 = Math.cos((startAngle * Math.PI) / 180);
         const y1 = Math.sin((startAngle * Math.PI) / 180);
         const x2 = Math.cos((endAngle * Math.PI) / 180);
         const y2 = Math.sin((endAngle * Math.PI) / 180);
-
         const pathData = `M ${x1} ${y1} A 1 1 0 ${largeArcFlag} 1 ${x2} ${y2} L 0 0`;
-
         startAngle = endAngle;
-
         return <path key={index} d={pathData} fill={COLORS[index % COLORS.length]} />;
       })}
     </svg>
