aeec2b194301cd2f08d6ac864d06a99dea9b6f10
Josh <joshuafwade@gmail.com>
2024-07-28 15:33:14 -0400

    update FE to handle test querying

--- types/index.ts
+++ types/index.ts
@@ -1,6 +1,38 @@
 // types/index.ts
+
 export interface Passage {
-    id: string;
-    text: string;
-    citation: string;
-  }
+  id: string;
+  text: string;
+  citation: string;
+}
+
+export interface Test {
+  id: string;
+  title: string;
+  description: string | null;
+  setName: string | null;
+  createdAt: string;
+  updatedAt: string;
+  questions: TestQuestion[];
+  _count?: {
+    questions: number;
+  };
+}
+
+export interface TestQuestion {
+  id: string;
+  testId: string;
+  questionId: string;
+  sequence: number;
+  question: Question;
+}
+
+export interface Question {
+  id: string;
+  questionID: string;
+  questionContent: string;
+  questionOptions: string;  
+  questionAnswerNotes: string;
+  contentCategory: string;
+  passageId?: string
+}
