f3a38fe840d522989e88513b2b4ab25c9c11060a
Josh <joshuafwade@gmail.com>
2024-08-06 00:07:55 -0400

    fix context ai error

--- app/(dashboard)/(routes)/home/page.tsx
+++ app/(dashboard)/(routes)/home/page.tsx
@@ -5,21 +5,24 @@ import ChatBot from "../../../../components/chatbot/ChatBot";
 import Image from "next/image";
 import rectangle from "../../../../public/homeimage.png";
 import { useUser } from "@clerk/nextjs";
+
 const Page = () => {
   const { user } = useUser();
+  const currentDate = new Date().toLocaleDateString();
+  const contextMessage = `Hello, my name is ${user?.firstName}. Today's date is ${currentDate}.`;
+
   return (
     <>
       <div className="bg-[#001326] min-h-[80vh] text-black flex">
         <div className="container mx-auto">
-          {" "}
           <div className="text-center mb-2">
-                    <h1 className="text-2xl font-bold text-white mt-10">
-                        Welcome {user?.firstName ?? "Guest"} to your MCAT!
-                    </h1>
-                </div>
+            <h1 className="text-2xl font-bold text-white mt-10">
+              Welcome {user?.firstName ?? "Guest"} to your MCAT!
+            </h1>
+          </div>
           <div className="w-full max-w-full grid grid-cols-3 gap-4 p-4">
             <div className="p-4 col-span-1">
-              <ChatBot />
+              <ChatBot context={contextMessage} />
             </div>
             <div className="p-4 col-span-2">
               <Image
