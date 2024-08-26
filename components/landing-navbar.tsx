2700e0b0c2742df39ec75e8d4061dd14fcd528d4
Prynce Karki <prynce@mymcat.ai>
2024-08-26 18:10:20 -0500

    changed it again

--- components/landing-navbar.tsx
+++ components/landing-navbar.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useAuth } from "@clerk/nextjs";
 import { useState, useEffect, useCallback, useMemo } from "react";
-import logo from "../public/logo.png";
+import logo from "../public/logo2.png";
 
 export const LandingNavbar = () => {
   const { isSignedIn } = useAuth();
