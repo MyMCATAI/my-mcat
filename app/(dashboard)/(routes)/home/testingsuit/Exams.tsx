b70a471f64d7c2dbaf363dc74d7b3354617bacb5
Prynce <shreyaskarki@Shreyass-MBP.attlocal.net>
2024-08-30 00:05:52 -0500

    I'm adding themes soon, this is just the beginning

--- app/(dashboard)/(routes)/home/testingsuit/Exams.tsx
+++ app/(dashboard)/(routes)/home/testingsuit/Exams.tsx
@@ -83,7 +83,7 @@ const Exams: React.FC<TestListingProps> = ({ tests }) => {
           <div 
             className="h-full bg-[#001226] rounded-[10px] p-4 flex flex-col relative" 
             style={{
-              backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 18, 38, 0.9)), url('/circuitpattern2.png')",
+              backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.95), rgba(0, 18, 38, 0.95)), url('/circuitpattern2.png')",
               backgroundSize: 'cover',
               backgroundRepeat: 'no-repeat',
               backgroundPosition: 'center',
@@ -179,7 +179,7 @@ const Exams: React.FC<TestListingProps> = ({ tests }) => {
           <div 
             className="h-[625px] overflow-y-auto rounded-lg p-4 bg-[#001226]"
             style={{
-              backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 18, 38, 0.7)), url('/circuitpattern2.png')",
+              backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.85), rgba(0, 18, 38, 0.85)), url('/circuitpattern2.png')",
               backgroundSize: 'cover',
               backgroundRepeat: 'no-repeat',
               backgroundPosition: 'center',
