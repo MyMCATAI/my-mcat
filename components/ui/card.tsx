ea67532eeb11d8eb4d532c60c0eb4d51e10fa161
shreyas-karki <kalypso@mymcat.ai>
2024-09-18 21:55:07 -0500

    need to make changes

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border bg-card text-card-foreground shadow-sm",
+      "rounded-lg bg-[--theme-mainbox-color] text-card-foreground shadow-sm",
       className
     )}
     {...props}
