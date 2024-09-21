a7e3a9ea3f4567d3fc0c3e704f707827c6b38357
Josh <joshuafwade@gmail.com>
2024-09-21 00:34:52 -0400

    add next auth server handling

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,5 @@
 import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
@@ -6,6 +7,19 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
       publicRoutes: ["/","/intro","/disclaimer","/cookiepolicy","/acceptableuse","/termsandconditions","/privacypolicy", "/api/webhook"],
       debug: true,
+      afterAuth(auth, req, evt) {
+            // Handle auth for public routes
+            if (auth.isPublicRoute) {
+              return NextResponse.next();
+            }
+        
+            // Handle other routes as before
+            if (!auth.userId && !auth.isPublicRoute) {
+              const signInUrl = new URL('/sign-in', req.url);
+              signInUrl.searchParams.set('redirect_url', req.url);
+              return NextResponse.redirect(signInUrl);
+            }
+          },
 });
 export const config = {
       matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
