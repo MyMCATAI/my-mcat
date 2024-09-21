703ac2b08708ec7ddbba53134d8396db6f7e0910
shreyas-karki <kalypso@mymcat.ai>
2024-09-21 04:39:41 -0500

    gamified and created an isometric game

---
+++ app/(dashboard)/(routes)/doctorsoffice/page.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import React from 'react';
+import ResourcesMenu from './ResourcesMenu';
+import OfficeContainer from './OfficeContainer';
+
+const DoctorsOfficePage: React.FC = () => {
+  return (
+    <div className="fixed inset-x-0 bottom-0 top-[5rem] flex bg-transparent text-white p-4">
+      <div className="flex w-full h-full max-w-[calc(100%-1rem)] max-h-full">
+        <div className="w-1/4 pr-4">
+          <ResourcesMenu />
+        </div>
+        <div className="w-3/4">
+          <OfficeContainer />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DoctorsOfficePage;
