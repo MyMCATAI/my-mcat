f849f0131df50e872938601d294a9affe4cf1145
shreyas-karki <kalypso@mymcat.ai>
2024-11-16 22:26:25 -0600

    fixed

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -6,56 +6,38 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: baseUrl,
-      lastModified: new Date(),
-      changeFrequency: 'daily',
       priority: 1,
     },
     {
       url: `${baseUrl}intro`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
       url: `${baseUrl}colleges`,
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
       priority: 0.7,
     },
     {
       url: `${baseUrl}disclaimer`,
-      lastModified: new Date(),
-      changeFrequency: 'yearly',
       priority: 0.5,
     },
     {
       url: `${baseUrl}cookiepolicy`,
-      lastModified: new Date(),
-      changeFrequency: 'yearly',
       priority: 0.5,
     },
     {
       url: `${baseUrl}acceptableuse`,
-      lastModified: new Date(),
-      changeFrequency: 'yearly',
       priority: 0.5,
     },
     {
       url: `${baseUrl}termsandconditions`,
-      lastModified: new Date(),
-      changeFrequency: 'yearly',
       priority: 0.5,
     },
     {
       url: `${baseUrl}privacypolicy`,
-      lastModified: new Date(),
-      changeFrequency: 'yearly',
       priority: 0.5,
     },
     {
       url: `${baseUrl}blog`,
-      lastModified: new Date(),
-      changeFrequency: 'daily',
       priority: 0.8,
     }
   ]
