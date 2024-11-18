fdaf5c8b90f62f0f84f3aa9f50f2c399324a9784
shreyas-karki <kalypso@mymcat.ai>
2024-11-18 01:50:46 -0600

    changing one line

--- components/landingpage/Faqs.tsx
+++ components/landingpage/Faqs.tsx
@@ -20,7 +20,7 @@ const Faqs = () => {
         { heading: "What if I do not like your app and think it's mediocre product?", text: `No one on our team is allowed to have high self esteem. Therefore, we welcome ANY and ALL criticism and will pay you for the privilege. `, icon: icon },
         { heading: "How does Kalypso differ from ChatGPT?", text: 'Kalypso is trained on the MCAT with dozens of textbooks and fine-tuned over hundreds of hours. He is trained on the latest in ITS research and programmed to be interesting and engaging. Who doesn’t love cats?', icon: icon },
         { heading: "How can I trust that your content is good?", text: `Prynce has written content for multiple test prep companies. We actually listen to feedback and modify our content to make it as close to AAMC-level as possible.`, icon: icon },
-        { heading: "Will you ever be free?", text: `Hell nah.`, icon: icon },
+        { heading: "I don't understand your financial model. Coins?", text: `Coins are an accountabiltiy measure. It's a way to track your progress and unlock features. The ideal student can unlock all features with 10 coins over time. The non-ideal student must buy coins.`, icon: icon },
         { heading: "What the heck is Studyverse?", text: `MyMCAT is a product offered by Studyverse. You are pilgrims for our future ambitions: a metaverse for studying for ALL students — from MCAT students to SAT students to college kids preparing for exams. Yes, we are that crazy.`, icon: icon },
     ];
 
