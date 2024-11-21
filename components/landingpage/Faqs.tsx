4281829f4afa29c0c69f3a8606b96dafe35226f5
shreyas-karki <kalypso@mymcat.ai>
2024-11-21 03:10:19 -0600

    studyverse medicine faq

--- components/landingpage/Faqs.tsx
+++ components/landingpage/Faqs.tsx
@@ -15,13 +15,13 @@ const Faqs = () => {
 
     const items = [
         { heading: "Why did you start this company?", text: 'Because our founder loves his students and wants to see them succeed, and thinks test prep companies are getting it wrong.', icon: icon },
-        { heading: "Who are your founders?", text: `Prynce is a 523 scorer and our product architect, front end engineer, AI wizard, and CEO. Josh is CTO, a computer science and tech genius who makes this crap actually work. Both of them work together with the hopes of changing education for the better`, icon: icon },
-        { heading: "If werewolves exist, then might there theoretically exist whenwolves?", text: `Bro, what?`, icon: icon },
+        { heading: "Who are your founders?", text: `Prynce is a 523 scorer and our product architect, front end engineer, AI wizard, and CEO. Josh is CTO, a computer science and tech genius who makes the dream into an actual reality. Both of them work together with the hopes of changing medical education for the better`, icon: icon },
+        { heading: "If werewolves exist, then might there theoretically exist whenwolves?", text: `Wild thought. Join our<a href="https://discord.gg/mXVkbzFq" target="_blank" rel="noopener noreferrer"> discord</a> and ask our community!`, icon: icon },
         { heading: "What if I do not like your app and think it's mediocre product?", text: `No one on our team is allowed to have high self esteem. Therefore, we welcome ANY and ALL criticism and will pay you for the privilege. `, icon: icon },
-        { heading: "How does Kalypso differ from ChatGPT?", text: 'Kalypso is trained on the MCAT with dozens of textbooks and fine-tuned over hundreds of hours. He is trained on the latest in ITS research and programmed to be interesting and engaging. Who doesn’t love cats?', icon: icon },
-        { heading: "How can I trust that your content is good?", text: `Prynce has written content for multiple test prep companies. We actually listen to feedback and modify our content to make it as close to AAMC-level as possible.`, icon: icon },
-        { heading: "I don't understand your financial model. Coins?", text: `Coins are an accountabiltiy measure. It's a way to track your progress and unlock features. The ideal student can unlock all features with 10 coins over time. The non-ideal student must buy coins.`, icon: icon },
-        { heading: "What the heck is Studyverse?", text: `MyMCAT is a product offered by Studyverse. You are pilgrims for our future ambitions: a metaverse for studying for ALL students — from MCAT students to SAT students to college kids preparing for exams. Yes, we are that crazy.`, icon: icon },
+        { heading: "How does Kalypso differ from ChatGPT?", text: 'Kalypso is fine-tuned over hundreds of hours over AAMC-type questions and passages. He is trained on the latest in ITS research and programmed to be interesting and engaging. And he is a cat. Who doesn’t love cats?', icon: icon },
+        { heading: "How can I trust that your content is good?", text: `Prynce has written content for multiple test prep companies. We have models trained on good AAMC-type questions and passages. We actually listen to feedback and modify our content to make it as close to AAMC-level as possible.`, icon: icon },
+        { heading: "I don't understand your financial model. Coins?", text: `Coins are an accountabiltiy measure. It's a way to track your progress and unlock features. The ideal student can unlock all features with 10 coins over time. And, for students who need a little more help, or lack a certain accountability, it will cost more.`, icon: icon },
+        { heading: "What the heck is Studyverse Medicine?", text: `MyMCAT is a product offered by Studyverse Medicine. We plan on developing a studyverse for all students in medicine, from premeds to medical students. Yes, we are that crazy.`, icon: icon },
     ];
 
     return (
