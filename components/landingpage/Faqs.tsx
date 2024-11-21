85a0ecd681ab9c14c4fe81d24e159a2a825cdbdc
shreyas-karki <kalypso@mymcat.ai>
2024-11-21 03:12:48 -0600

    studyverse medicine faq 3

--- components/landingpage/Faqs.tsx
+++ components/landingpage/Faqs.tsx
@@ -21,7 +21,7 @@ const Faqs = () => {
         { heading: "How does Kalypso differ from ChatGPT?", text: 'Kalypso is fine-tuned over hundreds of hours of AAMC-type questions and passages. He is trained on the latest in ITS research and programmed to be interesting and engaging. And he is a cat. Who doesn’t love cats?', icon: icon },
         { heading: "How can I trust that your content is good?", text: `Prynce has written content for multiple test prep companies. We have models trained on good AAMC-type questions and passages. We actually listen to feedback and modify our content to make it as close to AAMC-level as possible.`, icon: icon },
         { heading: "I don't understand your financial model. Coins?", text: `Coins are an accountabiltiy measure. It's a way to track your progress and unlock features. The ideal student can unlock all features with 10 coins over time. And, for students who need a little more help, or lack a certain accountability, it will cost more. This way, the most talented and hardest working members of our community are rewarded.`, icon: icon },
-        { heading: "What the heck is Studyverse Medicine?", text: `MyMCAT is a product offered by Studyverse Medicine. We plan on developing a studyverse for all students in medicine, from premeds to medical students. Yes, we are that crazy.`, icon: icon },
+        { heading: "What the heck is Studyverse Medicine?", text: `MyMCAT is a product offered by Studyverse Medicine. We plan on developing a studyverse for all students in medicine, from premeds to medical students to eventually doctors. Yes, we are that crazy.`, icon: icon },
     ];
 
     return (
