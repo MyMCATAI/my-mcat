b648cfbf5be54b7924bf5fbc67698e0ac8783eb1
Josh <joshuafwade@gmail.com>
2024-12-02 18:10:49 -0500

    Add welcome email on checkout

--- lib/server-utils.ts
+++ lib/server-utils.ts
@@ -2,6 +2,20 @@
 
 import { emailService } from "@/services/email/EmailService";
 
+export async function sendWelcomeEmail(userName: string, userEmail: string): Promise<boolean> {
+  try {
+    const result = await emailService.sendEmail({
+      to: userEmail,
+      template: "welcome",
+      data: { userName }
+    });
+    
+    return result.success;
+  } catch (error) {
+    console.error("Error sending welcome email:", error);
+    return false;
+  }
+}
 
 export async function sendReferralEmail(referrerName: string, friendEmail: string): Promise<boolean> {
     try {
