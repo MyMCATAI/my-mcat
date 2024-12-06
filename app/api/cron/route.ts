34f7863de07b6c32db6ce4a80953471d3fd1fbf8
Josh <joshuafwade@gmail.com>
2024-12-05 23:17:31 -0500

    update cron timing

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
+console.log(" Tried to run Job Ran at: ", new Date());
+
   const authHeader = request.headers.get("authorization");
   if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
     return new Response("Unauthorized", {
