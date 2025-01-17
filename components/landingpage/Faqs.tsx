97ec269c66d56239b9db8e6330afd448ae60dbcf
shreyas-karki <kalypso@mymcat.ai>
2025-01-17 11:19:03 -0600

    platform

--- components/landingpage/Faqs.tsx
+++ components/landingpage/Faqs.tsx
@@ -14,14 +14,14 @@ const Faqs = () => {
     };
 
     const items = [
-        { heading: "Why did you start this company?", text: 'Because our founder loves his students and wants to see them succeed, and thinks test prep companies are getting it wrong.', icon: icon },
+        { heading: "Why did you start this company?", text: 'Test prep companies have a habit of overpromising and underdelivering. We want to change that.', icon: icon },
         { heading: "Who are your founders?", text: `Prynce is a 523 scorer and our product architect, front end engineer, AI wizard, and CEO. Josh is CTO, a computer science and tech genius who brings dreams to life. Both of them work together with the hopes of changing medical education for the better`, icon: icon },
         { heading: "If werewolves exist, do whenwolves exist?", text: `Wild thought. <a href="https://discord.gg/DcHWnEu8Xb" class="text-blue-600 hover:text-blue-800" target="_blank" rel="noopener noreferrer">Join our discord</a> and ask our community! Surely, someone will know the answer.`, icon: icon },
         { heading: "What if I do not like your app and think it's mediocre product?", text: `No one on our team is allowed to have high self esteem. Therefore, we welcome ANY and ALL criticism — that way, we don't get too big headed. `, icon: icon },
         { heading: "How does Kalypso differ from ChatGPT?", text: 'Kalypso is fine-tuned over hundreds of hours of AAMC-type questions and passages. He is trained on the latest in ITS research and programmed to be interesting and engaging. And he is a cat. Who doesn’t love cats?', icon: icon },
         { heading: "How can I trust that your content is good?", text: `Prynce has written content for multiple test prep companies. We have models trained on good AAMC-type questions and passages. We actually listen to feedback and modify our content to make it as close to AAMC-level as possible.`, icon: icon },
-        { heading: "I don't understand your financial model. Coins?", text: `Coins are an accountability measure. It's a way to track your progress and unlock features. The ideal student can unlock all features with 10 coins over time. And, for students who need a little more help, or lack a certain accountability, it will cost more. This way, the most talented and hardest working members of our community are rewarded.`, icon: icon },
-        { heading: "What the heck is Studyverse Medicine?", text: `MyMCAT is a product offered by Studyverse Medicine. We plan on developing a studyverse for all students in medicine, from premeds to medical students to eventually doctors. Yes, we are that crazy.`, icon: icon },
+        { heading: "I don't understand what coins are?", text: `Coins are an accountability measure and a fun way to track your progress. The ideal student can unlock all features with 10 coins over time. And, for students who need a little more help, or lack a certain accountability, it will cost more. This way, the most talented and hardest working members of our community are rewarded.`, icon: icon },
+        { heading: "How do you compare to other test prep companies?", text: `MyMCAT is a premium product focused on delivering quality. We are free only because we believe in the power of community.`, icon: icon },
     ];
 
     return (
