dee82294ce4b6ffa0b65a983b5879e3d9e86483c
shreyas-karki <kalypso@mymcat.ai>
2025-01-17 11:13:23 -0600

    start-for-free

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -100,11 +100,11 @@ const LandingHero = () => {
             <div className="text-center">
               <h1 className="text-4xl md:text-5xl font-bold text-white font-krungthep mb-4">
                 <span>
-                  <span className="text-[#f2f64f]">Score</span> beyond <span className="text-[#f2f64f]">a 520</span>
+                  <span className="text-[#f2f64f]">Score</span> beyond <span className="text-[#f2f64f]">a 520.</span>
                 </span>
               </h1>
               <p className="text-2xl md:text-3xl text-white my-8">
-             Our AI-tutoring platform makes dream scores a reality.
+             Start for free on our tutoring platform.
               </p>
               <div className="flex justify-center">
                 <Link href="/sign-up">
