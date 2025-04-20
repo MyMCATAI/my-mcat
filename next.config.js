ab6072ce26a7b95cbaa1f6a804a8a3633a069497
Josh <joshuafwade@gmail.com>
2025-04-20 17:55:45 -0400

    remove reminder cron jobs from vercel json

--- next.config.js
+++ next.config.js
@@ -41,15 +41,15 @@ module.exports = withMDX({
           {
             key: 'Content-Security-Policy',
             value: [
-              "default-src 'self' https://*.clerk.io https://*.clerk.com https://*.clerk.accounts.dev https://*.stripe.com https://*.plausible.io https://*.vercel-scripts.com;",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.clerk.io https://*.clerk.com https://*.clerk.accounts.dev https://js.stripe.com https://plausible.io https://va.vercel-scripts.com https://tally.so blob: https://clerk.mymcat.ai;",
-              "worker-src 'self' blob: https://*.clerk.io https://*.clerk.com https://*.clerk.accounts.dev;",
-              "connect-src 'self' https://*.clerk.io https://*.clerk.com https://*.clerk.accounts.dev https://api.stripe.com https://plausible.io https://va.vercel-scripts.com https://cdn.jsdelivr.net https://clerk.mymcat.ai;",
-              "style-src 'self' 'unsafe-inline' https://*.clerk.io https://*.clerk.com https://*.clerk.accounts.dev https://cdn.jsdelivr.net;",
-              "img-src 'self' http://localhost:* https://img.youtube.com https://my-mcat.s3.us-east-2.amazonaws.com https://oaidalleapiprodscus.blob.core.windows.net https://b.thumbs.redditmedia.com https://*.clerk.io https://*.clerk.com https://*.clerk.accounts.dev data: blob:;",
-              "font-src 'self' https://*.clerk.io https://*.clerk.com https://*.clerk.accounts.dev data:;",
-              "frame-src 'self' https://js.stripe.com https://*.clerk.io https://*.clerk.com https://*.clerk.accounts.dev https://tally.so https://www.youtube.com;",
-              "media-src 'self' http://localhost:* https://*.clerk.io https://*.clerk.com https://*.clerk.accounts.dev data: blob: https://my-mcat.s3.us-east-2.amazonaws.com;",
+              "default-src 'self' https://*.clerk.io https://*.clerk.com https://*.clerk.accounts.dev https://clerk.mymcat.ai https://*.stripe.com https://*.plausible.io https://*.vercel-scripts.com;",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.clerk.io https://*.clerk.com https://*.clerk.accounts.dev https://clerk.mymcat.ai https://js.stripe.com https://plausible.io https://va.vercel-scripts.com https://tally.so blob:;",
+              "worker-src 'self' blob: https://*.clerk.io https://*.clerk.com https://*.clerk.accounts.dev https://clerk.mymcat.ai;",
+              "connect-src 'self' https://*.clerk.io https://*.clerk.com https://*.clerk.accounts.dev https://clerk.mymcat.ai/* https://api.stripe.com https://plausible.io https://va.vercel-scripts.com https://cdn.jsdelivr.net;",
+              "style-src 'self' 'unsafe-inline' https://*.clerk.io https://*.clerk.com https://*.clerk.accounts.dev https://clerk.mymcat.ai https://cdn.jsdelivr.net;",
+              "img-src 'self' http://localhost:* https://img.youtube.com https://my-mcat.s3.us-east-2.amazonaws.com https://oaidalleapiprodscus.blob.core.windows.net https://b.thumbs.redditmedia.com https://*.clerk.io https://*.clerk.com https://*.clerk.accounts.dev https://clerk.mymcat.ai data: blob:;",
+              "font-src 'self' https://*.clerk.io https://*.clerk.com https://*.clerk.accounts.dev https://clerk.mymcat.ai data:;",
+              "frame-src 'self' https://js.stripe.com https://*.clerk.io https://*.clerk.com https://*.clerk.accounts.dev https://clerk.mymcat.ai https://tally.so https://www.youtube.com;",
+              "media-src 'self' http://localhost:* https://*.clerk.io https://*.clerk.com https://*.clerk.accounts.dev https://clerk.mymcat.ai data: blob: https://my-mcat.s3.us-east-2.amazonaws.com;",
               "object-src 'none';",
               "base-uri 'self';"
             ].join(' ')
